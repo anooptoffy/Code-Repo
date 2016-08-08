b23b82e0f567494c4078ba783bb48229c105dd46
Anoop Toffy <anoop.toffy90@gmail.com>
2016-08-08 18:52:40 +0530

    memory analysis

--- Semester_1/CS501/Day2/exercise2.c
+++ Semester_1/CS501/Day2/exercise2.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 void main(){
-	int i,j,k,n,a[10000000];
-	n=1000;
+	int i,j,k,n,a[1000000];
+	n=100;
 	for (i=0;i<n;++i)
 		for (j=0;j<n;++j)
 			for (k=0;k<n;++k)
 				a[i*n*n+j*n+k]=2*i*j*k+1;
+	scanf("%d",&i);
 }
---
+++ Semester_1/CS501/Day2/intsize.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	size_t n;
+	printf("%ld",sizeof(int));
+	return 0;
+}
