995f9afc8f975cfab5a15132cd44e52c57d088d7
Anoop Toffy <anoop.toffy90@gmail.com>
2016-08-17 15:20:46 +0530

    sorting

---
+++ Semester_1/CS501/Day5/SortingAlgorithms/InsertionSort/InsertionSort.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*
+ *  Insertion Sort
+ *
+ *  Number of comparisons is 0(n^2)
+ *  If sorted (Best case) 0(n)
+ *
+ *
+ */
+
+void swap(int *a, int *b){
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void insertionSort(int *A,int sz){
+	for(int j = 1; j < sz; j++){
+		int t = A[j];
+		int i = j - 1;
+		while( i >= 0 && A[i] > t){	
+			A[i+1] = A[i];
+			i--;
+		}	
+		A[i + 1] = t;					
+	}
+}
+
+int main(){
+	printf("\n");
+	int a[] = {5, 7, 4, 2, 40, 53, 99, 44, 1};
+	int size;
+	size = sizeof(a)/sizeof(a[0]);
+	insertionSort(a,size);
+	printf("Sorted Array\n");
+	for(int i = 0; i < size; i++)
+		printf("%d ", a[i]);
+	printf("\n");
+	return 0;
+}
