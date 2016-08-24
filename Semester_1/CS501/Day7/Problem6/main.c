1ab353bbb79c601f17759747d6345c753cd34d5c
Anoop Toffy <anoop.toffy90@gmail.com>
2016-08-25 02:14:18 +0530

    naive

--- Semester_1/CS501/Day7/Problem6/main.c
+++ Semester_1/CS501/Day7/Problem6/main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
+/*
+ *
+ *
+ * max(aj - ai)
+ * i <= j <= i+l
+ * using naive approach that has complexity O(n*l)
+ *
+ */
+
 int main(int argc, char *argv[]){
+	int a[] = {10, 11, 7, 10, 6};
+ 	int size = sizeof(a)/sizeof(a[0]);
+	int l = 3;
+	int max = -1;
+	for(int i = 0; i < size - l; i++){
+		for(int j = i; j < (i+l); j++){
+			if(a[j] - a[i] > max)
+ 				max = a[j] -  a[i];
+ 			if(a[j] < a[i])
+ 				i = j-1;
+		}
+		printf("When to buy, %d Day at price %d\n", i, a[i]);
+	}
 
 	return 0;
 }
