174fb8a00c535f081e8cf22bb0f64cbf520c8868
Anoop Toffy <anoop.toffy90@gmail.com>
2016-08-25 02:15:32 +0530

    imp

---
+++ Semester_1/CS501/Day7/Problem6/main_imp.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+ *
+ *
+ * max(aj - ai)
+ * i <= j <= i+l
+ * using naive approach that has complexity O(n*l) Improving Further Is O(n) possible ?
+ *
+ */
+
+int main(int argc, char *argv[]){
+	int a[] = {10, 11, 7, 10, 6};
+ 	int size = sizeof(a)/sizeof(a[0]);
+	int l = 3;
+	int max = -1;
+
+
+	return 0;
+}
