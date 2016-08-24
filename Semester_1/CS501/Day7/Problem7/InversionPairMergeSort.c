d61279d492d06beae5178d8d31d255f1e75fb426
Anoop Toffy <anoop.toffy90@gmail.com>
2016-08-24 21:55:42 +0530

    inversion pair

--- Semester_1/CS501/Day7/Problem7/InversionPairMergeSort.c
+++ Semester_1/CS501/Day7/Problem7/InversionPairMergeSort.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 /*
- * Inversion Pair   Can we do better ? O(nlogn)  Try using Merge Sort
+ * Inversion Pair   Can we do better ? O(nlogn)  Try using Merge Sort logic
  * 
- *  a0, a1, a2, ... , an-1 is in the increasing order
- *  if i < j and ai > aj forms the inversion pair
+ *  Let a0, a1, a2, ... , an-1  be a arbitrary sequence of numbers
+ *  if i < j and ai > aj then [ai,aj] forms the inversion pair
  *
  *  Unsorted
+ *  Links : http://www.geeksforgeeks.org/counting-inversions/
  */
 
 int c;
+
+int comparator(const void *a, const void *b){
+	int f = *(int *)a;
+	int s = *(int *)b;
+	return s-f; //desc
+}
 
 int MIP(int a[], int f, int mid, int l){
 	if(a[f] > a[l])
@@ -20,8 +27,8 @@ int MIP(int a[], int f, int mid, int l){
 int IP(int a[], int i, int j){
 	if(i < j){
 		int mid = (i+j)/2;
-		IP(a,i,mid);
-		IP(a,mid+1,j);
+		//IP(a,i,mid);
+		//IP(a,mid+1,j);
 		MIP(a,i,mid,j);
 	}
 }
@@ -30,6 +37,11 @@ int main(int argc, char const *argv[])
 {
 	int a[] = {4, 5, 2, 6, 1};
 	int size = sizeof(a)/sizeof(a[0]);
+	qsort((void *)a,size, sizeof(a[0]),comparator);
+	for (int i = 0; i < size; ++i)
+	{
+		printf("%d ", a[i]);
+	}
 	IP(a,0,size - 1);
 	printf("\nThe number of inversion pairs are, %d\n", c);
 
