a40dc3bd41850101e2e22d6ecadb5c758679c06d
Anoop Toffy <anoop.toffy90@gmail.com>
2016-09-03 18:47:20 +0530

    fix for program4

--- Semester_1/CS501/Day8/SmallSubArrayContainNumbers/algorithm.c
+++ Semester_1/CS501/Day8/SmallSubArrayContainNumbers/algorithm.c
@@ -14,37 +14,51 @@ int main(int argc, char const *argv[])
 	int size = sizeof(a)/sizeof(a[0]);
 
 	int k = 4;
-	int StartIndex = 0, EndIndex = 0;
+	int StartIndex = 0, EndIndex = size;
 	int c = 0;
 	int aC[100] = {0};
+	int length = 0; // This should be minimum.
 
 	for (int i = 0; i < size; ++i)
 	{
-		if (a[i] <= k && aC[a[i]] >= 0)
+		if (a[i] <= k)
 		{
+			aC[a[i]]++;
 			if(aC[a[i]] == 1)
 				c++;
-			aC[a[i]]++;
-			EndIndex = i;
+			
+			//EndIndex = i;
 		}
-
+		//printf("[DEBUG] The value of i is %d\n", i);
+		//printf("[DEBUG] The c value is %d\n", c);
 		if(c == k){
-			int k = StartIndex;
+			int l = StartIndex;
 
 			while(1){
-				if(aC[a[k]] >= 2){
-					aC[a[k]]--;
-					k++;
+				if(a[l] > k)
+				{
+					l++;
+					continue;
+				}
+				//printf("The count of k (%d) in aC[%d] is %d\n",k,k, aC[a[k]]);
+				if(aC[a[l]] > 1){
+					aC[a[l]]--;
+					l++;
 				}
 				else
 					break;
 			}
-			StartIndex = k;
+			if((i - l + 1) < (EndIndex - StartIndex + 1)){
+				StartIndex = l;
+				EndIndex = i;
+			}
+			//printf("The value of StartIndex is %d\n", StartIndex);
 			//break;
 		}
 	}
 
 	printf("Smallest sub array that contains all %d numbers is %d\n",k,(EndIndex - StartIndex + 1) );
+	printf("StartIndex %d, EndIndex %d\n",StartIndex,EndIndex );
 
 	return 0;
 }			
